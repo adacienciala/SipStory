1baf27e8591506c5ea679c6bb3217c65b832a655
Ada Cieńciała <adrianna.cienciala@htdhealth.com>
2025-11-08 22:10:54 +0100

    fix: linter

--- src/components/tasting-detail/DeleteConfirmDialog.tsx
+++ src/components/tasting-detail/DeleteConfirmDialog.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
@@ -7,7 +7,7 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 interface DeleteConfirmDialogProps {
   isOpen: boolean;
